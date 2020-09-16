a57ef87c996a2c7f0193556482e780a7b34d8ad1
Rob Day <robd@uber.com>
2020-09-16 18:28:28 -0400

    initial commit

---
+++ src/start.js
@@ -0,0 +1,43 @@
+import { app, BrowserWindow } from 'electron'
+
+import { join } from 'path'
+import { format } from 'url'
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  mainWindow.loadURL(
+    process.env.ELECTRON_START_URL ||
+      format({
+        pathname: join(__dirname, '/../public/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      })
+  )
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
