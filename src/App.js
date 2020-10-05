1c1a59be2ae8b3209f99a14db31cbcb08a98a46c
Rob Day <robd@uber.com>
2020-10-04 22:29:02 -0400

    It all compiles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemCount from './components/item-count'
+import {ItemCategory} from './models/index'
 
 import getStashContent from './client/get-stash-content'
 
 function App() {
   let json = getStashContent('yousillygoose', 1);
   console.log(json);
+
+  const itemCounts = {
+    [ItemCategory.Helmet]: 5,
+    [ItemCategory.Belt]: 1,
+    [ItemCategory.Armor]: 2,
+    [ItemCategory.Gloves]: 3,
+    [ItemCategory.Boots]: 10,
+    [ItemCategory.OneHandedWeapon]: 3,
+    [ItemCategory.TwoHandedWeapon]: 2,
+    [ItemCategory.Ring]: 1,
+    [ItemCategory.Amulet]: 6
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +37,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ItemCount itemCounts={itemCounts}/>
     </div>
   );
 }
