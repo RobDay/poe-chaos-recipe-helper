82c53b03ee127ba64ca75a9b988d763f4045f81b
Rob Day <robd@uber.com>
2020-10-09 08:48:24 -0400

    robotjs click through success

--- src/components/stash-overlay.tsx
+++ src/components/stash-overlay.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { styled } from "styletron-react";
 import StashItemOverlay from "./stash-item-overlay";
-import {StashItem} from '../models/index'
+import { ItemCategory, ItemType, StashItem } from "../models/index";
+
+//TODO: Filter in the recipe generator for rate
+const getTempStashItem = (id: string, x: number, y: number): StashItem => {
+  return {
+    type: ItemType.Rare,
+    category: ItemCategory.Amulet,
+    name: "Tacos",
+    ilvl: 2,
+    width: 2,
+    height: 3,
+    identified: true,
+    id,
+    x,
+    y,
+  };
+};
+
+const stashItems = [
+  getTempStashItem("a", 0, 0),
+  getTempStashItem("b", 4, 4),
+  getTempStashItem("c", 7, 4),
+  getTempStashItem("d", 10, 10),
+  getTempStashItem("e", 13, 10),
+  getTempStashItem("f", 17, 17),
+];
 
 const Container = styled("div", {
   backgroundColor: "red",
-  position: "relative"
+  position: "relative",
 });
 
 const INVENTORY_WIDTH = 1134;
@@ -15,33 +40,36 @@ enum StashWidth {
 }
 
 type PropsType = {
-  stashItems: StashItem[]
-}
+  // stashItems: StashItem[];
+  // density: StashWidth.Quad;
+};
 
-function StashOverlay() {
+function StashOverlay(props: PropsType) {
+  const onOverlayItemClick = (stashItem: StashItem) => {};
 
-  const onOverlayItemClick = (stashItem: StashItem) => {
-  }
+  const getSizeInPixels = (size: number) => {
+    // Check quad
+    const cellCount: number = StashWidth.Quad;
+    return `${(size * INVENTORY_WIDTH) / cellCount}px`;
+  };
 
-    const renderStashItems = () => {
-
+  const renderStashItems = () => {
+    const itemOverlays = stashItems.map((item) => {
       return (
-<StashItemOverlay
-        width={`${(INVENTORY_WIDTH / 24) * 2}px`}
-        height={`${(INVENTORY_WIDTH / 24) * 3}px`}
-        left={`${(INVENTORY_WIDTH / 24) * 2}px`}
-        top={`${(INVENTORY_WIDTH / 24) * 2}px`}
-        color="orange"
-        onStashItemClicked={onOverlayItemClick}
-      />
-      )
-    }
-      
-  return (
-    <Container>
-      {renderStashItems()}
-    </Container>
-  );
+        <StashItemOverlay
+          width={getSizeInPixels(item.width)}
+          height={getSizeInPixels(item.height)}
+          left={getSizeInPixels(item.x)}
+          top={getSizeInPixels(item.y)}
+          color="orange"
+          onStashItemClicked={onOverlayItemClick}
+        />
+      );
+    });
+    return itemOverlays;
+  };
+
+  return <Container>{renderStashItems()}</Container>;
 }
 
 export default StashOverlay;
--- src/components/view-manager.tsx
+++ src/components/view-manager.tsx
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, RouteComponentProps } from "react-router-dom";
 import MainComponent from "./primary-component";
 import StashOverlay from "./stash-overlay";
+
+function Empty() {
+  return (<div onClick={() => console.log("empty on click")} style={{backgroundColor: "red", position:"absolute"}}>tacostacostacostacostacostacostacostacostacostacostacostacos tacostacostacostacostacostacostacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos tacostacostacos</div>)
+}
+
 class ViewManager extends Component {
   static Views() {
     const result: { [key: string]: any } = {
-        mainWindow: <MainComponent />,
+      mainWindow: <MainComponent />,
       overlay: <StashOverlay />,
+      empty: <Empty />
     };
 
     return result;
@@ -14,9 +20,6 @@ class ViewManager extends Component {
 
   static View(props: RouteComponentProps<any>) {
     let name = props.location.search.substr(1) as string;
-    console.log(props.location);
-    console.log(`Name is ${name}`)
-    console.log(window.location.href);
     let view = ViewManager.Views()[name];
     if (view == null) throw new Error(`View ${name} is undefined`);
     return view;
