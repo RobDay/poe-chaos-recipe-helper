a2c90876ae537cf91642770339b64d25567e47ba
Rob Day <robd@uber.com>
2020-10-11 23:42:42 -0400

    Pushing data fetching into shared communication channels to share updates between the windows

--- src/components/item-count/item-count-list.tsx
+++ src/components/item-count/item-count-list.tsx
@@ -10,7 +10,7 @@ import RecipeManager, { StashItemCounts } from "../../recipe-manager";
 
 const boxSize = "45px";
 type PropsType = {
-  //   itemCounts: StashItemCounts;
+  itemCounts: StashItemCounts;
 };
 
 const SizedItemCountCell = styled(ItemCountCell, {
@@ -24,67 +24,67 @@ const FlexContainer = styled("div", {
 });
 
 export default function ItemCountList(props: PropsType) {
-  const [itemCounts, setItemCounts] = useState<StashItemCounts | undefined>();
+  //   const [itemCounts, setItemCounts] = useState<StashItemCounts | undefined>();
 
-  const getItemCounts = async () => {
-    const stashItems = await getStashContent("", 5);
-    const recipeManager = new RecipeManager(stashItems);
-    const itemStats = recipeManager.generateItemStatistics();
-    console.log("setting counts to");
-    console.log(itemStats);
-    setItemCounts(itemStats);
-  };
+  //   const getItemCounts = async () => {
+  //     const stashItems = await getStashContent("", 5);
+  //     const recipeManager = new RecipeManager(stashItems);
+  //     const itemStats = recipeManager.generateItemStatistics();
+  //     console.log("setting counts to");
+  //     console.log(itemStats);
+  //     setItemCounts(itemStats);
+  //   };
 
-  useEffect(() => {
-    getItemCounts();
-  }, []);
+  //   useEffect(() => {
+  //     getItemCounts();
+  //   }, []);
 
-  if (itemCounts) {
+  if (props.itemCounts) {
     return (
       <FlexContainer style={{ height: boxSize }}>
         <SizedItemCountCell
           itemName="🎩"
-          itemCount={itemCounts.HELMET}
+          itemCount={props.itemCounts.HELMET}
           color={CATEGORY_COLORS[ItemCategory.Helmet]}
         />
         <SizedItemCountCell
           itemName="🥋"
-          itemCount={itemCounts.BELT}
+          itemCount={props.itemCounts.BELT}
           color={CATEGORY_COLORS[ItemCategory.Belt]}
         />
         <SizedItemCountCell
           itemName="🧥"
-          itemCount={itemCounts.ARMOR}
+          itemCount={props.itemCounts.ARMOR}
           color={CATEGORY_COLORS[ItemCategory.Armor]}
         />
         <SizedItemCountCell
           itemName="🥊"
-          itemCount={itemCounts.GLOVES}
+          itemCount={props.itemCounts.GLOVES}
           color={CATEGORY_COLORS[ItemCategory.Gloves]}
         />
         <SizedItemCountCell
           itemName="🥾"
-          itemCount={itemCounts.BOOTS}
+          itemCount={props.itemCounts.BOOTS}
           color={CATEGORY_COLORS[ItemCategory.Boots]}
         />
         <SizedItemCountCell
           itemName="⚔️"
-          itemCount={itemCounts.ONE_HANDED_WEAPON}
+          itemCount={props.itemCounts.ONE_HANDED_WEAPON}
           color={CATEGORY_COLORS[ItemCategory.OneHandedWeapon]}
         />
         <SizedItemCountCell
           itemName="🏹"
-          itemCount={itemCounts.TWO_HANDED_WEAPON}
+          itemCount={props.itemCounts.TWO_HANDED_WEAPON}
           color={CATEGORY_COLORS[ItemCategory.TwoHandedWeapon]}
         />
         <SizedItemCountCell
           itemName="💍"
-          itemCount={itemCounts.RING}
+          itemCount={props.itemCounts.RING}
           color={CATEGORY_COLORS[ItemCategory.Ring]}
         />
         <SizedItemCountCell
           itemName="📿"
-          itemCount={itemCounts.AMULET}
+          itemCount={props.itemCounts.AMULET}
           color={CATEGORY_COLORS[ItemCategory.Amulet]}
         />
       </FlexContainer>
@@ -93,5 +93,3 @@ export default function ItemCountList(props: PropsType) {
 
   return <FlexContainer style={{ height: boxSize }}></FlexContainer>;
 }
-
-function ItemCountListContainer(props: PropsType) {}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,64 +1,68 @@
 export enum ItemCategory {
-    Helmet = "HELMET",
-    Belt = "BELT",
-    Armor = "ARMOR",
-    Gloves = "GLOVES",
-    Boots = "BOOTS",
-    OneHandedWeapon = "ONE_HANDED_WEAPON",
-    TwoHandedWeapon = "TWO_HANDED_WEAPON",
-    Ring = "RING",
-    Amulet = "AMULET",
-    // Jewel = "Jewel",
-    // Splinter = "Splinter",
-    Unknown = "Unknown"
+  Helmet = "HELMET",
+  Belt = "BELT",
+  Armor = "ARMOR",
+  Gloves = "GLOVES",
+  Boots = "BOOTS",
+  OneHandedWeapon = "ONE_HANDED_WEAPON",
+  TwoHandedWeapon = "TWO_HANDED_WEAPON",
+  Ring = "RING",
+  Amulet = "AMULET",
+  // Jewel = "Jewel",
+  // Splinter = "Splinter",
+  Unknown = "Unknown",
 }
 
 export type ItemCategoryKeys = keyof ItemCategory;
 
 // TODO: Add influence
 
-export enum ItemType  {
-    Normal = 0,
-    Magic = 1,
-    Rare = 2,
-    Unique = 3,
-    Gem = 4,
-    Currency = 5,
-    DivinationCard = 6,
-    QuestItem = 7,
-    Prophecy = 8,
-    Relic = 9
+export enum ItemType {
+  Normal = 0,
+  Magic = 1,
+  Rare = 2,
+  Unique = 3,
+  Gem = 4,
+  Currency = 5,
+  DivinationCard = 6,
+  QuestItem = 7,
+  Prophecy = 8,
+  Relic = 9,
 }
 
 export type StashItem = {
-    type: ItemType;
-    category: ItemCategory
-    name: string;
-    x: number;
-    y: number;
-    ilvl: number;
-    width: number;
-    height: number;
-    identified: boolean;
-    id: string;
-}
+  type: ItemType;
+  category: ItemCategory;
+  name: string;
+  x: number;
+  y: number;
+  ilvl: number;
+  width: number;
+  height: number;
+  identified: boolean;
+  id: string;
+};
 
 export type StashAPIContentItem = {
-    w: number;
-    h: number;
-    name: string;
-    x: number;
-    y: number;
-    typeLine: string;
-    frameType: number;
-    inventoryId: string;
-    ilvl: number;
-    identified: boolean;
-    id: string;
-}
+  w: number;
+  h: number;
+  name: string;
+  x: number;
+  y: number;
+  typeLine: string;
+  frameType: number;
+  inventoryId: string;
+  ilvl: number;
+  identified: boolean;
+  id: string;
+};
 
 export type StashAPIResponse = {
-    numTabs: number;
-    quadTab: boolean;
-    items: StashAPIContentItem[];
-}
+  numTabs: number;
+  quadTab: boolean;
+  items: StashAPIContentItem[];
+};
+
+export type RefreshStashPayload = {
+  items: StashItem[];
+};
